c0a67629aadee28d873ad8aa859b3cc7062704bf
Shashank <shashankambagade@gmail.com>
2026-03-06 19:07:44 +0530

    Branding updates

--- src/components/sections/home/HomeHero.jsx
+++ src/components/sections/home/HomeHero.jsx
@@ -2,9 +2,6 @@
 
 import Image from "next/image";
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import "swiper/css";
 import DownArrow from "../../../../public/hero-down-arrow.png";
 import { motion } from "framer-motion";
 
@@ -14,27 +11,6 @@ export default function HomeHero({ data }) {
 
   const heading = data?.heading || "";
   const shortHeading = data?.short_heading || "";
-  const clientSlider = data?.client_slider || [];
-
-  const statsSlides = clientSlider.filter(
-    (item) => item.logo || item.statistics
-  );
-  const testimonialSlides = clientSlider.filter((item) => item.testimonial);
-
-  const [activeIndex, setActiveIndex] = React.useState(0);
-
-  let logoSwiperRef = null;
-  let testiSwiperRef = null;
-
-  // ✔ SYNC TESTIMONIAL + LOGO HIGHLIGHT EVERY 3s
-  React.useEffect(() => {
-    const interval = setInterval(() => {
-      setActiveIndex((prev) => (prev + 1 >= statsSlides.length ? 0 : prev + 1));
-      testiSwiperRef?.slideToLoop(activeIndex);
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, [activeIndex, statsSlides.length]);
 
   return (
     <section className="relative w-full  overflow-hidden hero">
@@ -108,143 +84,7 @@ export default function HomeHero({ data }) {
         </div>
       </div>
 
-      {/* CLIENT LOGOS + TESTIMONIAL */}
-      <div className="absolute bottom-0 w-full backdrop-blur-xl bg-white/10 border-t border-white/20 py-9">
-        <div className="web-width mx-auto md:flex items-center gap-6 hidden md:block">
-          {/* LEFT — LOGO TICKER */}
-          <div className="md:max-w-[70%] ticker-wrapper">
-            <Swiper
-              modules={[Autoplay]}
-              onSwiper={(swiper) => (logoSwiperRef = swiper)}
-              autoplay={{
-                delay: 2500, // REQUIRED
-                reverseDirection: true, // Slide LEFT → RIGHT
-                disableOnInteraction: false,
-              }}
-              loop={true}
-              allowTouchMove={false}
-              speed={800}
-              slidesPerView={4}
-              spaceBetween={30}
-              initialSlide={2}
-              breakpoints={{
-                320: { slidesPerView: 1 },
-                640: { slidesPerView: 2 },
-                1024: { slidesPerView: 3 },
-                1280: { slidesPerView: 4 },
-              }}
-            >
-              {statsSlides.map((item, index) => {
-                const isActive = index === activeIndex;
 
-                return (
-                  <SwiperSlide key={index}>
-                    <div
-                      className={`flex flex-col items-center transition-all`}
-                    >
-                      {/* Logo */}
-                      {item.logo?.url && (
-                        <Image
-                          src={item.logo.url}
-                          alt="client logo"
-                          width={90}
-                          height={38}
-                          className="object-contain"
-                        />
-                      )}
-
-                      {/* Show statistics ONLY for active logo */}
-                      {item.statistics && (
-                        <p className="caption-text mt-3 text-white">
-                          {item.statistics}
-                        </p>
-                      )}
-                    </div>
-                  </SwiperSlide>
-                );
-              })}
-            </Swiper>
-          </div>
-
-          {/* RIGHT — VERTICAL TESTIMONIAL SLIDER */}
-          <div className="md:max-w-[30%] h-[80px] overflow-hidden">
-            <Swiper
-              modules={[Autoplay]}
-              direction={"vertical"}
-              onSwiper={(swiper) => (logoSwiperRef = swiper)}
-              autoplay={{
-                delay: 2500, // REQUIRED
-                reverseDirection: true,
-                disableOnInteraction: false,
-              }}
-              loop={true}
-              allowTouchMove={false}
-              speed={800}
-              className="h-full"
-            >
-              {testimonialSlides.map((item, index) => (
-                <SwiperSlide key={index}>
-                  <div className="text-white caption-text max-w-[320px] mx-auto">
-                    <p className="">{item.testimonial}</p>
-                    <p className="mt-2 font-semibold">{item.client_name}</p>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
-        </div>
-        <div className="px-6 md:hidden">
-          <Swiper
-            modules={[Autoplay]}
-            autoplay={{
-              delay: 2500, // REQUIRED
-              reverseDirection: true, // Slide LEFT → RIGHT
-              disableOnInteraction: false,
-            }}
-            loop={true}
-            allowTouchMove={true}
-            speed={800}
-            slidesPerView={1}
-            spaceBetween={30}
-            onSlideChange={(swiper) => {
-              setActiveIndex(swiper.realIndex);
-              testiSwiperRef?.slideToLoop(swiper.realIndex);
-            }}
-          >
-            {statsSlides.map((item, index) => {
-              const isActive = index === activeIndex;
-
-              return (
-                <SwiperSlide key={index}>
-                  <div className={`flex flex-col items-center transition-all`}>
-                    {/* Logo */}
-                    {item.logo?.url && (
-                      <Image
-                        src={item.logo.url}
-                        alt="client logo"
-                        width={90}
-                        height={38}
-                        className="object-contain"
-                      />
-                    )}
-
-                    {/* Show statistics ONLY for active logo */}
-                    {item.statistics && (
-                      <p className="caption-text mt-3 text-white">
-                        {item.statistics}
-                      </p>
-                    )}
-                    <div className="text-white text-center">
-                      <p className="mt-2 opacity-90">{item.testimonial}</p>
-                      <p className="mt-2 font-semibold">{item.client_name}</p>
-                    </div>
-                  </div>
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-        </div>
-      </div>
     </section>
   );
 }
